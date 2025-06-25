fc5de82f6a01fbb272d19911ca1dce728f62a0c0
fAntomlt <tsalechovas@gmail.com>
2025-06-25 18:20:39 +0300

    Register Form organism created

---
+++ src/components/organisms/RegisterForm.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import Input from '../atoms/Input';
+import Button from '../atoms/Button';
+
+const RegisterForm = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail]     = useState('');
+  const [password, setPassword] = useState('');
+  const [profilePic, setProfilePic] = useState('');
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleRegister}>
+      <Input
+        type="text"
+        placeholder="Slapyvardis"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        required
+      />
+      <Input
+        type="email"
+        placeholder="El. paštas"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <Input
+        type="password"
+        placeholder="Slaptažodis"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+      <Input
+        type="text"
+        placeholder="Nuotraukos URL (neprivaloma)"
+        value={profilePic}
+        onChange={(e) => setProfilePic(e.target.value)}
+      />
+      <Button type="submit">Sukurti paskyrą</Button>
+    </form>
+  );
+};
+
+export default RegisterForm;
