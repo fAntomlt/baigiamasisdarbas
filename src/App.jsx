d57879f466e200f27237b41cfe9a8517deaa44e4
fAntomlt <tsalechovas@gmail.com>
2025-06-25 17:27:51 +0300

    Routes added and registerpage work started

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AuthPage from './pages/AuthPage';
+import RegisterPage from './pages/RegisterPage';
 
 const App = () => {
-  return <AuthPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<AuthPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
