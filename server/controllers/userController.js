e65c78b61b8020166eadba5ce060e7844766898b
fAntomlt <tsalechovas@gmail.com>
2025-06-25 22:46:18 +0300

    Small change to user constant

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,7 +4,7 @@ const updateUser = async (req, res) => {
     const { username, email, profilePic, newPassword } = req.body;
 
     try {
-        const user = await User.findById(req.user.id);
+        const user = await User.findById(req.user._id);
         if (!user) return res.status(404).json({ message: 'User not found' });
         if (username) user.username = username;
         if (email) user.email = email;
