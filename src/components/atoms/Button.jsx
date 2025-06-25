71fed456cafaa0e80992b2e872eb2ad3147ee126
fAntomlt <tsalechovas@gmail.com>
2025-06-25 18:19:52 +0300

    Button atom created

---
+++ src/components/atoms/Button.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  width: 100%;
+  padding: 0.75rem;
+  background-color: #42b72a;
+  color: white;
+  border: none;
+  font-weight: bold;
+  border-radius: 6px;
+  font-size: 1rem;
+  cursor: pointer;
+`;
+
+export default Button;
