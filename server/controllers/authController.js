43ce051e9b4f2486287882d69853dc8dc83444e9
fAntomlt <tsalechovas@gmail.com>
2025-06-25 16:46:29 +0300

    Auth controller added

---
+++ server/controllers/authController.js
@@ -0,0 +1,8 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+    expiresIn: '7d',
+  });
+};
