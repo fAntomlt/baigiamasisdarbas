c77531caf63f2fe83368239f998eec8c114f9738
fAntomlt <tsalechovas@gmail.com>
2025-06-25 20:19:37 +0300

    Code made more readable

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -10,15 +10,13 @@ const initialState = {
 const authReducer = (state, action) => {
   switch (action.type) {
     case 'LOGIN':
-      localStorage.setItem('lifebook_user', JSON.stringify(action.payload.user));
-      localStorage.setItem('lifebook_token', action.payload.token);
-      return { user: action.payload.user, token: action.payload.token };
-
+        localStorage.setItem('lifebook_user', JSON.stringify(action.payload.user));
+        localStorage.setItem('lifebook_token', action.payload.token);
+        return { user: action.payload.user, token: action.payload.token };
     case 'LOGOUT':
-      localStorage.removeItem('lifebook_user');
-      localStorage.removeItem('lifebook_token');
-      return { user: null, token: null };
-
+        localStorage.removeItem('lifebook_user');
+        localStorage.removeItem('lifebook_token');
+        return {user: null, token: null};
     default:
       return state;
   }
@@ -28,9 +26,9 @@ const AuthProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, initialState);
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
-      {children}
+        {children}
     </AuthContext.Provider>
   );
 };
 
-export { AuthContext, AuthProvider };
+export {AuthContext, AuthProvider};
