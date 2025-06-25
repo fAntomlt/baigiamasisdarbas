8267412b94bad217ae54f44ecfd1f9a08a55b4a2
fAntomlt <tsalechovas@gmail.com>
2025-06-25 16:48:49 +0300

    User registration controller logic added

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,7 +2,33 @@ const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
 const generateToken = (userId) => {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+  return jwt.sign({id: userId}, process.env.JWT_SECRET, {
     expiresIn: '7d',
   });
+};
+
+const registerUser = async (req, res) => {
+  const {username, email, password, profilePic} = req.body;
+
+  try {
+    const userExists = await User.findOne({email});
+    if (userExists) return res.status(400).json({message: 'Toks vartotojas jau egzistuoja'});
+
+    const user = await User.create({
+      username,
+      email,
+      password,
+      profilePic,
+    });
+
+    res.status(201).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      profilePic: user.profilePic,
+      token: generateToken(user._id),
+    });
+  } catch (err) {
+    res.status(500).json({ message: 'Uzsiregistruoti nepavyko, del:', error: err.message });
+  }
 };
