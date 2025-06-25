e67e317059322d5f3ee99bc08bbd6ff687985a40
fAntomlt <tsalechovas@gmail.com>
2025-06-25 17:09:59 +0300

    Added authreducer and AuthProvider for global state

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -5,3 +5,27 @@ const AuthContext = createContext();
 const initialState = {
   user: JSON.parse(localStorage.getItem('lifebook_user')) || null,
 };
+
+const authReducer = (state, action) => {
+    switch (action.type){
+        case 'LOGIN':
+            localStorage.setItem('lifebook_user', JSON.stringify(action.payload));
+            return {user: action.payload};
+        case 'LOGOUT':
+            localStorage.removeItem('lifebook_user');
+            return {user: null};
+        default:
+            return state;
+    }
+};
+
+const AuthProvider = ({children}) => {
+    const [state, dispatch] = useReducer(authReducer, initialState);
+    return (
+        <AuthContext.Provider value={{...state, dispatch}}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export {AuthContext, AuthProvider};
