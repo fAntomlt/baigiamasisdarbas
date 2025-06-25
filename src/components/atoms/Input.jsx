c86f4efb434749396d1916372a253c2146d48fcc
fAntomlt <tsalechovas@gmail.com>
2025-06-25 18:19:30 +0300

    Input atom created

---
+++ src/components/atoms/Input.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  width: 100%;
+  margin-bottom: 1rem;
+  padding: 0.75rem;
+  border: 1px solid #ccc;
+  border-radius: 6px;
+`;
+
+export default Input;
